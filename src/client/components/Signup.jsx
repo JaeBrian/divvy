cb2e773a80512ad03fa14020f81c9557fc34e3dd
Nancy Huang <nancyhuang@nancyhmac.lan>
2023-12-19 15:08:40 -0800

    created signup form

--- src/client/components/Signup.jsx
+++ src/client/components/Signup.jsx
@@ -1,8 +1,58 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import '../styles/Login.scss';
 
 const Signup = () => {
-    // Signup page
+  const [newUserFirstName, setNewUserFirstName] = useState('');
+  const [newUserLastName, setNewUserLastName] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  return (
+    <div className='mainContainer'>
+      <div className='titleContainer'>
+        Sign Up
+      </div>
+      <div className='inputContainer'>
+        <input
+          value={newUserFirstName}
+          placeholder='First Name'
+          onChange={e => setNewUserFirstName(e.target.value)}
+          className='inputBox'
+        />
+        <label className='errorLabel'>{usernameError}</label>
+      </div>
+      <div className='inputContainer'>
+        <input
+          value={newUserLastName}
+          placeholder='Last Name'
+          onChange={e => setNewUserLastName(e.target.value)}
+          className='inputBox'
+        />
+        <label className='errorLabel'>{passwordError}</label>
+      </div>
+      <div className='inputContainer'>
+        <input
+          className='inputButton'
+          type='button'
+          onClick={onLoginClick}
+          value='Login'
+        />
+      </div>
+      <div className='footerContainer'>
+        <div className='footerText'>
+          Forgot password
+        </div>
+        <input
+          className='inputButton'
+          type='button'
+          onClick={onSignupClick}
+          value='Signup'
+        />
+      </div>
+    </div>
+  );
+
 }
 
 export default Signup;
